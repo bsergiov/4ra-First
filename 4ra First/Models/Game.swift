b70374bd6db2dec42d950e1d7b2f98ae6e990141
bsergiov <81264661+bsergiov@users.noreply.github.com>
2022-05-16 14:56:48 +0200

    added models

---
+++ 4ra First/Models/Game.swift
@@ -0,0 +1,15 @@
+//
+//  Game.swift
+//  4ra First
+//
+//  Created by SV on 16.05.2022.
+//
+
+class Games {
+    let players: [Player] = []
+    let name: String = ""
+}
+
+class Player {
+    
+}
