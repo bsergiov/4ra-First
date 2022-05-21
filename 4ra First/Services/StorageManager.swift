ba4fc1e8369b6e1759d09352c33911aac61000f4
bsergiov <81264661+bsergiov@users.noreply.github.com>
2022-05-21 09:35:24 +0200

    StorageManager.swift refactor added logic for work realm

--- 4ra First/Services/StorageManager.swift
+++ 4ra First/Services/StorageManager.swift
@@ -5,6 +5,64 @@
 //  Created by SV on 16.05.2022.
 //
 
+import RealmSwift
+
 class StorageManager {
     
+    static let shared = StorageManager()
+    
+    let realm = try! Realm()
+    
+    private init() { }
+    
+    func save(_ game: Game) {
+        write {
+            realm.add(game)
+        }
+    }
+    
+    func update(_ game: Game, for time: Int) {
+        write {
+            game.timeGame = time
+        }
+    }
+    
+    func update(_ game: Game, currentStatus: Int) {
+        write {
+            game.currentStatusGame = currentStatus
+        }
+    }
+    
+    func update(_ player: Player, point: Int) {
+        write {
+            player.points = point
+        }
+    }
+    
+    func update(forFavorite player: Player) {
+        write {
+            player.isFavorite = true
+        }
+    }
+    
+    func delete(_ game: Game) {
+        write {
+            realm.delete(game.players)
+            realm.delete(game)
+        }
+    }
+    
+    func fetchGame(completion: @escaping (Results<Game>) -> Void) {
+        
+        let games = realm.objects(Game.self)
+        completion(games)
+    }
+    
+    private func write(completion: () -> Void) {
+        do {
+            try realm.write { completion() }
+        } catch  {
+            print("Error write or read realm in db: \(error)")
+        }
+    }
 }
